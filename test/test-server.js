75f25b8e95c9ae647aed0954d6d0b0f61b007037
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-11-05 19:02:13 +0800

    created travis configuration file

--- test/test-server.js
+++ test/test-server.js
@@ -150,3 +150,86 @@ describe("Shopping List", function() {
     );
   });
 });
+
+
+describe('Recipes', function() {
+  
+  before(function() {
+    runServer();
+  });
+
+  after(function() {
+    closeServer();
+  });
+
+  // get
+
+  it('should list items on GET', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+
+      expect(res.body.length).to.be.at.least(1);
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.forEach(function(item) {
+        expect(item).to.be.a('object');
+        expect(item).to.include.keys(expectedKeys);
+        expect(item.ingredients).to.be.a('array');
+      });
+    });
+  });
+  
+  // post
+  it('should create item on POST', function() {
+    const newItem = {
+      name: 'Yakult Soju Mix', 
+      ingredients: ['Yakult', 'Rice Soju', 'Sprite']
+    };
+    return chai.request(app)
+    .post('/recipes')
+    .send(newItem)
+    .then(function(res) {
+      expect(res).to.have.status(201);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.include.keys('id', 'name', 'ingredients');
+      expect(res.body.id).to.not.equal(null);
+      expect(res.body).to.deep.equal(Object.assign(newItem, {id:res.body.id}));
+    });
+  });
+
+  // put
+  it('should update item on PUT', function() {
+    const updateData = {
+      name: 'Mezcal Mule',
+      ingredients: ['Mezcal', 'Ginger beer', 'Lime juice', 'Agave nectar']
+    };
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      updateData.id = res.body[0].id;
+      return chai.request(app)
+      .put(`/recipes/${updateData.id}`)
+      .send(updateData)
+    })
+    .then(function(res) {
+      expect(res).to.have.status(204);
+    });
+  });
+
+  // delete
+  it('should delete item on DELETE', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+        .delete(`/shopping-list/${res.body[0].id}`);
+    })
+    .then(function(res) {
+      expect(res).to.have.status(204);
+    });
+  });
+});
